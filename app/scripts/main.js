d4433b719bb2ba173b3639d2aab4097a6349cf9d
Yousuf El-Miniawi <yousuf.el-miniawi@soprasteria.com>
2019-04-05 15:30:03 +0100

    started patterns to design system by using forms

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -82,3 +82,50 @@ $(document).ready(function(){
         $('#back-to-top').tooltip('show');
 
 });
+
+// TURN OFF NUMBER INPUT BUTTONS
+JavaScript
+jQuery(document).ready( function($) {
+
+    // Disable scroll when focused on a number input.
+    $('form').on('focus', 'input[type=number]', function(e) {
+        $(this).on('wheel', function(e) {
+            e.preventDefault();
+        });
+    });
+
+    // Restore scroll on number inputs.
+    $('form').on('blur', 'input[type=number]', function(e) {
+        $(this).off('wheel');
+    });
+
+    // Disable up and down keys.
+    $('form').on('keydown', 'input[type=number]', function(e) {
+        if ( e.which == 38 || e.which == 40 )
+            e.preventDefault();
+    });
+
+});
+
+//DISABLE SCROLLING ON NUMBER INPUTS
+jQuery(document).ready( function($) {
+
+    // Disable scroll when focused on a number input.
+    $('form').on('focus', 'input[type=number]', function(e) {
+        $(this).on('wheel', function(e) {
+            e.preventDefault();
+        });
+    });
+
+    // Restore scroll on number inputs.
+    $('form').on('blur', 'input[type=number]', function(e) {
+        $(this).off('wheel');
+    });
+
+    // Disable up and down keys.
+    $('form').on('keydown', 'input[type=number]', function(e) {
+        if ( e.which == 38 || e.which == 40 )
+            e.preventDefault();
+    });
+
+});
